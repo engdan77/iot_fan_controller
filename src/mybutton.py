e32a99a4ecb5d30134d3ba34b5fb95ca471e6074
Daniel Engvall <daniel@engvalls.eu>
2020-02-10 21:31:42 +0100

    Adding a blocking check button click count function

--- src/mybutton.py
+++ src/mybutton.py
@@ -1,6 +1,8 @@
 from machine import Pin
 from ucollections import deque
 import uasyncio as asyncio
+import utime
+from myled import blink_int
 
 
 class MyButton:
@@ -25,3 +27,21 @@ class MyButton:
             return self.pressed_queue.popleft()
         except (ValueError, IndexError):
             return False
+
+
+def blocking_count_clicks(button_pin=14, timeout=5, debounce_ms=5, sleep_ms=10):
+    press_count = 0
+    number_iterations = (timeout * 1000) / sleep_ms
+    for _ in range(number_iterations):
+        p = Pin(button_pin, Pin.IN, Pin.PULL_UP)
+        if bool(p.value()) is False:
+            being_pressed = []
+            for d in range(20):
+                being_pressed.append(bool(p.value()))
+                utime.sleep_ms(debounce_ms)
+            if not any(being_pressed):
+                blink_int()
+                press_count += 1
+                print('button pressed')
+        utime.sleep_ms(sleep_ms)
+    return press_count
