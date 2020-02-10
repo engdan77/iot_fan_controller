b437eb00e5cb6ddccd55c9fab9d93244a9be7b82
Daniel Engvall <daniel@engvalls.eu>
2020-02-10 21:31:18 +0100

    Adding module for blinking internal led

---
+++ src/myled.py
@@ -0,0 +1,10 @@
+from machine import Pin
+import utime
+
+
+def blink_int(led_pin=2, count=1, on_time=500):
+    for _ in count:
+        led = Pin(led_pin, Pin.OUT)
+        led.value(False)
+        utime.sleep_ms(on_time)
+        led.value(True)
