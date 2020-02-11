c18231966c400ae189b1776b936d5dc504af5041
Daniel Engvall <daniel@engvalls.eu>
2020-02-11 08:50:25 +0100

    Correcting range when blinking

--- src/myled.py
+++ src/myled.py
@@ -3,7 +3,7 @@ import utime
 
 
 def blink_int(led_pin=2, count=1, on_time=500):
-    for _ in count:
+    for _ in range(count):
         led = Pin(led_pin, Pin.OUT)
         led.value(False)
         utime.sleep_ms(on_time)
